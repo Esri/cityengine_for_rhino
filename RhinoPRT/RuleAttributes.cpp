91b6a26ec37029a61ee28dd4f766901888e42e0c
Loris <lor11212>
2020-07-20 16:33:31 +0200

    Started to implement annotation support.

--- RhinoPRT/RuleAttributes.cpp
+++ RhinoPRT/RuleAttributes.cpp
@@ -7,6 +7,45 @@ std::wstring getNiceName(const std::wstring& attrName) {
 		return pcu::removeImport(pcu::removeStyle(attrName));
 }
 
+bool annotCompatibleWithType(AttributeAnnotation annot, prt::AnnotationArgumentType type) {
+	switch (type) {
+	case prt::AnnotationArgumentType::AAT_BOOL:
+		return annot == AttributeAnnotation::A_ENUM;
+	case prt::AnnotationArgumentType::AAT_INT:
+	case prt::AnnotationArgumentType::AAT_FLOAT:
+		return annot == A_ENUM || annot == A_RANGE;
+	case prt::AnnotationArgumentType::AAT_STR:
+		return annot != A_RANGE;
+	default:
+		return false;
+	}
+}
+
+AnnotationBase* getAnnotationObject(const wchar_t* annotName, const prt::Annotation* an, prt::AnnotationArgumentType attrType) {
+	if (!std::wcscmp(annotName, ANNOT_COLOR) && annotCompatibleWithType(A_COLOR, attrType)) {
+		return new AnnotationBase(A_COLOR);
+	}
+	else if (!std::wcscmp(annotName, ANNOT_ENUM)) {
+		if(attrType == prt::AAT_BOOL) return new AnnotationEnum<bool>(an);
+		if (attrType == prt::AAT_FLOAT) return new AnnotationEnum<double>(an);
+		if (attrType == prt::AAT_STR) return new AnnotationEnum<const wchar_t*>(an);
+		if (attrType == prt::AAT_INT) return new AnnotationEnum<int>(an);
+		return new AnnotationBase(A_NOANNOT);
+	}
+	else if (!std::wcscmp(annotName, ANNOT_RANGE) && annotCompatibleWithType(A_RANGE, attrType)) {
+		return new AnnotationRange(an);
+	}
+	else if (!std::wcscmp(annotName, ANNOT_DIR) && annotCompatibleWithType(A_DIR, attrType)) {
+		return new AnnotationBase(A_DIR);
+	}
+	else if(!std::wcscmp(annotName, ANNOT_FILE) && annotCompatibleWithType(A_FILE, attrType)) {
+		return new AnnotationFile(an);
+	}
+	else {
+		return new AnnotationBase(A_NOANNOT);
+	}
+}
+
 RuleAttributes getRuleAttributes(const std::wstring& ruleFile, const prt::RuleFileInfo& ruleFileInfo) {
 	RuleAttributes ra;
 
@@ -18,13 +57,47 @@ RuleAttributes getRuleAttributes(const std::wstring& ruleFile, const prt::RuleFi
 	for (size_t i = 0; i < ruleFileInfo.getNumAttributes(); ++i) {
 		const prt::RuleFileInfo::Entry* attr = ruleFileInfo.getAttribute(i);
 
+		if (attr->getNumParameters() != 0) continue;
+
 		RuleAttribute ruleAttr;
 		ruleAttr.mFullName = attr->getName();
 		ruleAttr.mRuleFile = mainCgaRuleName; // TODO: Only support 1 cgb file for now
 		ruleAttr.mNickname = getNiceName(ruleAttr.mFullName);
 		ruleAttr.mType = attr->getReturnType();
 
-		//TODO: implement prt::Annotation support.
+		// prt::Annotation
+		bool hidden = false;
+
+		for (size_t a = 0; a < attr->getNumAnnotations(); ++a) {
+			const prt::Annotation* an = attr->getAnnotation(a);
+			const wchar_t* anName = an->getName();
+
+			if (!(std::wcscmp(anName, ANNOT_HIDDEN))) {
+				hidden = true;
+			}
+			else if (!std::wcscmp(anName, ANNOT_ORDER)) {
+				if (an->getNumArguments() >= 1 && an->getArgument(0)->getType() == prt::AAT_FLOAT) {
+					ruleAttr.order = static_cast<int>(an->getArgument(0)->getFloat());
+				}
+			}
+			else if (!std::wcscmp(anName, ANNOT_GROUP)) {
+				for (int argIdx = 0; argIdx < an->getNumArguments(); ++argIdx) {
+					if (an->getArgument(argIdx)->getType() == prt::AAT_STR) {
+						ruleAttr.groups.push_back(an->getArgument(argIdx)->getStr());
+					}
+					else if (argIdx == an->getNumArguments() - 1 &&
+						an->getArgument(argIdx)->getType() == prt::AAT_FLOAT) {
+						ruleAttr.groupOrder = static_cast<int>(an->getArgument(argIdx)->getFloat());
+					}
+				}
+			}
+			else {
+				ruleAttr.mAnnotation = getAnnotationObject(anName, an, attr->getReturnType());
+			}
+			
+		}
+
+		if (hidden) continue;
 
 		ra.push_back(ruleAttr);
 		if (DBG) LOG_DBG << ruleAttr;
--- RhinoPRT/RuleAttributes.h
+++ RhinoPRT/RuleAttributes.h
@@ -8,6 +8,8 @@ struct IUnknown; // Workaround for "combaseapi.h(229): error C2187: syntax error
 #include "utils.h"
 #include "Logger.h"
 
+#include <limits>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -21,11 +23,125 @@ constexpr auto DBG = true;
 
 typedef struct _RuleAttribute RuleAttribute;
 
+constexpr const wchar_t* ANNOT_RANGE = L"@Range";
+constexpr const wchar_t* ANNOT_ENUM = L"@Enum";
+constexpr const wchar_t* ANNOT_HIDDEN = L"@Hidden";
+constexpr const wchar_t* ANNOT_COLOR = L"@Color";
+constexpr const wchar_t* ANNOT_DIR = L"@Directory";
+constexpr const wchar_t* ANNOT_FILE = L"@File";
+constexpr const wchar_t* ANNOT_ORDER = L"@Order";
+constexpr const wchar_t* ANNOT_GROUP = L"@Group";
+
+constexpr int ORDER_FIRST = std::numeric_limits<int>::lowest();
+constexpr int ORDER_NONE = std::numeric_limits<int>::infinity();
+
+constexpr const wchar_t* MIN_KEY = L"min";
+constexpr const wchar_t* MAX_KEY = L"max";
+
+using AttributeGroup = std::vector<std::wstring>;
+using AttributeGroupOrder = std::map<AttributeGroup, int>;
+
+	
+enum AttributeAnnotation { A_COLOR = 0, A_RANGE, A_ENUM, A_FILE, A_DIR, A_NOANNOT };
+
+
+class AnnotationBase {
+public:
+	AnnotationBase(AttributeAnnotation annot): mAnnotType(annot) {}
+
+	virtual ON_SimpleArray<int> getAnnotArguments() { return ON_SimpleArray<int>(mAnnotType); };
+
+protected:
+	AttributeAnnotation mAnnotType;
+};
+
+class AnnotationRange : public AnnotationBase {
+public:
+	AnnotationRange(const prt::Annotation* an): AnnotationBase(A_RANGE) {
+		for (int argIdx = 0; argIdx < an->getNumArguments(); ++argIdx) {
+			const prt::AnnotationArgument* arg = an->getArgument(argIdx);
+			const wchar_t* key = arg->getKey();
+			if (std::wcscmp(key, MIN_KEY) == 0) {
+				mMin = arg->getFloat();
+			}
+			else if (std::wcscmp(key, MAX_KEY) == 0) {
+				mMax = arg->getFloat();
+			}
+		}
+	}
+
+private:
+	double mMin;
+	double mMax;
+};
+
+/// Enum annotations can be strings, numbers or bools
+template<class T>
+class AnnotationEnum : public AnnotationBase {
+public:
+	AnnotationEnum(const prt::Annotation* an) : AnnotationBase(A_NOANNOT) {
+		LOG_WRN << L"Rule type incompatible with enum.";
+	}
+};
+
+template<>
+class AnnotationEnum<bool>: public AnnotationBase {
+public:
+	AnnotationEnum(const prt::Annotation* an) : AnnotationBase(A_ENUM) {
+		for (int argIdx = 0; argIdx < an->getNumArguments(); ++argIdx) {
+			mEnums.Append(an->getArgument(argIdx)->getBool());
+		}
+	}
+
+private:
+	ON_SimpleArray<bool> mEnums;
+};
+
+template<>
+class AnnotationEnum<const wchar_t*> : public AnnotationBase {
+public:
+	AnnotationEnum(const prt::Annotation* an) : AnnotationBase(A_ENUM) {
+		for (int argIdx = 0; argIdx < an->getNumArguments(); ++argIdx) {
+			mEnums.Append(an->getArgument(argIdx)->getStr());
+		}
+	}
+
+private:
+	ON_SimpleArray<const wchar_t*> mEnums;
+};
+
+template<>
+class AnnotationEnum<double> : public AnnotationBase {
+public:
+	AnnotationEnum(const prt::Annotation* an) : AnnotationBase(A_ENUM) {
+		for (int argIdx = 0; argIdx < an->getNumArguments(); ++argIdx) {
+			mEnums.Append(an->getArgument(argIdx)->getFloat());
+		}
+	}
+
+private:
+	ON_SimpleArray<double> mEnums;
+};
+
+
+class AnnotationFile : public AnnotationBase {
+public:
+	AnnotationFile(const prt::Annotation* an) : AnnotationBase(A_FILE) {}
+};
+
+AnnotationBase* getAnnotationObject(const wchar_t* annotName, const prt::Annotation* an, prt::AnnotationArgumentType attrType);
+
 struct _RuleAttribute {
 	std::wstring mRuleFile;	/// The cga file in which this rule attribute is defined.
 	std::wstring mFullName;	/// The attribute full name given to grasshopper.
 	std::wstring mNickname;	/// The attribute short name displayed in the gh node.
 	prt::AnnotationArgumentType mType;
+
+	AttributeGroup groups;
+	int order = ORDER_NONE;
+	int groupOrder = ORDER_NONE;
+
+	AnnotationBase * mAnnotation;
 };
 
 using RuleAttributes = std::vector<RuleAttribute>;
