862c8e0192eca39a3b18a67c08cbd14cfdef3263
lor11212 <laiulfi@esri.com>
2021-04-26 10:52:44 +0200

    use of version number variables for the the yak packages

--- create_package.py
+++ create_package.py
@@ -36,34 +36,34 @@ package_type = 'both'
 if len(sys.argv) > 1:
     package_type = sys.argv[1]
 
+# Parse file "version.h" to get major, minor, revision, build numbers
+version_file: str = os.path.join(curr_dir, "RhinoPRT", "version.h")
+
+pattern = re.compile(r'^#define (?P<version>\w+)\s+(?P<number>\d+)$')
+
+v_major = 0
+v_minor = 0
+v_revision = 0
+v_build = 0
+
+with open(version_file, 'r') as file:
+    lines: list = file.readlines()
+    for line in lines:
+        result = pattern.match(line)
+        if result:
+            version = result.group('version')
+            number = result.group('number')
+
+            if version == 'VERSION_MAJOR':
+                v_major = number
+            elif version == 'VERSION_MINOR':
+                v_minor = number
+            elif version == 'VERSION_REVISION':
+                v_revision = number
+            elif version == 'VERSION_BUILD':
+                v_build = number
+
 if package_type == 'both' or package_type == 'rhi':
-    # Parse file "version.h" to get major, minor, revision, build numbers
-    version_file: str = os.path.join(curr_dir, "RhinoPRT", "version.h")
-
-    pattern = re.compile(r'^#define (?P<version>\w+)\s+(?P<number>\d+)$')
-
-    v_major = 0
-    v_minor = 0
-    v_revision = 0
-    v_build = 0
-
-    with open(version_file, 'r') as file:
-        lines: list = file.readlines()
-        for line in lines:
-            result = pattern.match(line)
-            if result:
-                version = result.group('version')
-                number = result.group('number')
-
-                if version == 'VERSION_MAJOR':
-                    v_major = number
-                elif version == 'VERSION_MINOR':
-                    v_minor = number
-                elif version == 'VERSION_REVISION':
-                    v_revision = number
-                elif version == 'VERSION_BUILD':
-                    v_build = number
-
     version_str: str = f'v{v_major}.{v_minor}.{v_revision}.{v_build}'
 
     with zipfile.ZipFile(os.path.join(package_dir, f'Puma_{version_str}.rhi'), 'w') as myZip:
@@ -77,12 +77,14 @@ if package_type == 'both' or package_type == 'yak':
     os.system('cmd /c ""C:\Program Files\Rhino 7\System\Yak.exe" build"')
 
     # create rhino 7 yak package
-    shutil.copyfile(os.path.join(build_dir, 'puma-0.6.0-rh6_34-any.yak'),
-                    os.path.join(build_dir, 'puma-0.6.0-rh7-any.yak'))
+    shutil.copyfile(os.path.join(build_dir, f'puma-{v_major}.{v_minor}.{v_revision}-rh6_34-any.yak'),
+                    os.path.join(build_dir, f'puma-{v_major}.{v_minor}.{v_revision}-rh7-any.yak'))
 
     # move both yak to the package_output directory
-    shutil.move('puma-0.6.0-rh6_34-any.yak', os.path.join(package_dir, 'puma-0.6.0-rh6_34-any.yak'))
-    shutil.move('puma-0.6.0-rh7-any.yak', os.path.join(package_dir, 'puma-0.6.0-rh7-any.yak'))
+    shutil.move(f'puma-{v_major}.{v_minor}.{v_revision}-rh6_34-any.yak',
+                os.path.join(package_dir, f'puma-{v_major}.{v_minor}.{v_revision}-rh6_34-any.yak'))
+    shutil.move(f'puma-{v_major}.{v_minor}.{v_revision}-rh7-any.yak',
+                os.path.join(package_dir, f'puma-{v_major}.{v_minor}.{v_revision}-rh7-any.yak'))
 
     # cleaning
     os.remove('manifest.yml')
