75a645efb21baa1cf9f09bf31826348fda62036c
Loris Aiulfi <laiulfi@esri.com>
2022-09-28 16:00:55 +0200

    Removed commented code

--- PumaGrasshopper/Properties/AssemblyInfo.cs
+++ PumaGrasshopper/Properties/AssemblyInfo.cs
@@ -21,14 +21,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2c95f9dd-5756-46f5-8c23-9e0a182b8c74")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.0")]
-// [assembly: AssemblyFileVersion("1.0.0.0")]
