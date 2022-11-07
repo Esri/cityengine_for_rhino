023bde2c66bd1056dbc9c87935bb2271b0f6f1bb
Loris Aiulfi <laiulfi@esri.com>
2022-11-07 15:56:01 +0100

    Regenerated files

--- PumaGrasshopper/Properties/AssemblyVersion.cs
+++ PumaGrasshopper/Properties/AssemblyVersion.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
